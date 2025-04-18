409aff1bab3f1b6ac88980f96213c2ed06872e7c
Hope Philip <idoreyenphilip@gmail.com>
2025-04-18 14:54:24 +0100

    fix the useArticle context

--- src/hooks/useArticle.ts
+++ src/hooks/useArticle.ts
@@ -1,17 +1,12 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext } from "react";
 import { ArticleContext } from "../context/ArticleContext";
 
 export function useArticle() {
   const context = useContext(ArticleContext);
-  const [selectedArticle, setSelectedArticle] = useState<any | null>(null);
 
   if (context === undefined) {
     throw new Error("useArticle must be used within an ArticleProvider");
   }
 
-  useEffect(() => {
-    console.log("Current Selected Article:", selectedArticle); // Debug log
-  }, [selectedArticle]);
-
-  return { ...context, selectedArticle, setSelectedArticle };
+  return context;
 }
